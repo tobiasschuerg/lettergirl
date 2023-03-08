3d9dd4084d2d57e849347e6007c731d8d3bd5d99
Tobias Schürg <tobiasschuerg@gmail.com>
2023-03-08 23:47:33 +0100

    move letter creation into out folder

--- letter_util.py
+++ letter_util.py
@@ -1,4 +1,5 @@
 import subprocess
+from pathlib import Path
 
 
 def compile_letter(tex_template: str) -> bytes:
@@ -7,10 +8,12 @@ def compile_letter(tex_template: str) -> bytes:
     :param tex_template: tex file
     :return: pdf as bytes
     """
-    with open('letter_temp.tex', 'w', encoding="utf-8") as f:
+    temp_tex_file = Path('out/letter_temp.tex')
+    temp_tex_file.parent.mkdir(exist_ok=True, parents=True)
+    with open(temp_tex_file, 'w', encoding="utf-8") as f:
         f.write(tex_template)
-    subprocess.run(['pdflatex', 'letter_temp.tex'], check=True)
+    subprocess.run(['pdflatex', 'letter_temp.tex'], check=True, cwd=temp_tex_file.parent)
     # Read the generated PDF file and return it as a response
-    with open('letter_temp.pdf', 'rb') as f:
+    with open(temp_tex_file.with_name('letter_temp.pdf'), 'rb') as f:
         pdf_bytes = f.read()
     return pdf_bytes
