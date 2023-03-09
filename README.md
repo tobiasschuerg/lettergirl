c07ce9bb44a9c8f4f29a27463a594a369d492b73
Tobias Schürg <tobiasschuerg@gmail.com>
2023-03-09 01:02:44 +0100

    configure and run prettier

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # lettergirl
- Easily and fast create nice termination letters
 
+Easily and fast create nice termination letters
 
 ## Usage
+
 Spin up docker container
+
 ```bash
 docker build -t lettergirl .                  # build container
 docker run --rm -it -p 8000:8000 lettergirl   # runcontainer
 ```
 
-2. 
+2.
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,29 +1,29 @@
-const searchInput = document.querySelector('#search-input');
-const cardsContainer = document.querySelector('#card-container');
+const searchInput = document.querySelector("#search-input");
+const cardsContainer = document.querySelector("#card-container");
 
 // Add event listener for the keyup event
-searchInput.addEventListener('keyup', () => {
-    const searchTerm = searchInput.value.toLowerCase().trim();
+searchInput.addEventListener("keyup", () => {
+  const searchTerm = searchInput.value.toLowerCase().trim();
 
-    // Loop through all the cards
-    cardsContainer.querySelectorAll('.card-item').forEach(card => {
-        const name = card.dataset.name.toLowerCase();
+  // Loop through all the cards
+  cardsContainer.querySelectorAll(".card-item").forEach((card) => {
+    const name = card.dataset.name.toLowerCase();
 
-        // If the card name contains the search term, show the card; otherwise, hide it
-        if (name.includes(searchTerm)) {
-            card.classList.remove('d-none');
-        } else {
-            card.classList.add('d-none');
-        }
-    });
+    // If the card name contains the search term, show the card; otherwise, hide it
+    if (name.includes(searchTerm)) {
+      card.classList.remove("d-none");
+    } else {
+      card.classList.add("d-none");
+    }
+  });
 });
 
 // Add event listener for the input event
-searchInput.addEventListener('input', () => {
-    if (searchInput.value.trim() === '') {
-        // If the search input is empty, show all the cards
-        cardsContainer.querySelectorAll('.card-item').forEach(card => {
-            card.classList.remove('d-none');
-        });
-    }
+searchInput.addEventListener("input", () => {
+  if (searchInput.value.trim() === "") {
+    // If the search input is empty, show all the cards
+    cardsContainer.querySelectorAll(".card-item").forEach((card) => {
+      card.classList.remove("d-none");
+    });
+  }
 });
