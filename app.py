2fefc63a459a8ca1c30fcad6c243ff493e466cbe
Tobias Schürg <tobiasschuerg@gmail.com>
2023-03-10 23:55:12 +0100

    switch to bootstrap 5

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def generate_letter():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- letter_util.py
+++ letter_util.py
@@ -12,9 +12,9 @@ def compile_letter(tex_template: str) -> bytes:
     temp_tex_file.parent.mkdir(exist_ok=True, parents=True)
     with open(temp_tex_file, "w", encoding="utf-8") as f:
         f.write(tex_template)
-    subprocess.run(
-        ["pdflatex", "letter_temp.tex"], check=True, cwd=temp_tex_file.parent
-    )
+    subprocess.run(["pdflatex", "letter_temp.tex"],
+                   check=True,
+                   cwd=temp_tex_file.parent)
     # Read the generated PDF file and return it as a response
     with open(temp_tex_file.with_name("letter_temp.pdf"), "rb") as f:
         pdf_bytes = f.read()
